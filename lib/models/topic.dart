6ba5c7508555b6f158b4a3848daf1dfe1e555765
datsabahandude <mohd.noor.amlee@gmail.com>
2024-05-04 22:59:48 +0800

    pros & cons update

--- lib/models/topic.dart
+++ lib/models/topic.dart
@@ -52,18 +52,16 @@ final List<TopicCategory> theTopics = [
     category: "Pros",
     topics: [
       Topic(
-        text:
-            'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
-      )
+          text:
+              '1. Precise Temperature Control\n2. Enhanced Flavor Retention\n3. Tender and Juicy Results\n4. Convenience\n5. Reduced Risk of Overcooking')
     ],
   ),
   TopicCategory(
     category: "Cons",
     topics: [
       Topic(
-        text:
-            'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
-      )
+          text:
+              '1. Time-consuming\n2. Equipment Requirements\n3. Lack of Browning\n4. Texture Concerns\n5. Food Safety Considerations')
     ],
   ),
 ];
